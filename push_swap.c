8543d3f0053103ebe22ce67ede3857a4d9cc55ea
Oussama Khourssa <okhourss@c3r9p7.1337.ma>
2025-01-24 19:39:03 +0100

    first commit

---
+++ push_swap.c
@@ -0,0 +1,174 @@
+// // #include <stdio.h>
+// // #include <stdlib.h>
+
+// // int check_is_number(char *arg)
+// // {
+// // 	int i = 0;
+// // 	while (arg[i])
+// // 	{
+// // 		if (!(arg[i] >= '0' && arg[i] <= '9'))
+// // 			return 0;
+// // 		i++;
+// // 	}
+// // 	return 1;
+// // }
+// // void print_arr(int *arr, int size)
+// // 	{
+// // 		int i = 0;
+// // 		// int size = sizeof(arr) / sizeof(int);
+// // 		while (i < size)
+// // 		{
+// // 			printf("arr[%d] -- > %d\n", i ,arr[i]);
+// // 			i++;
+// // 		}
+// // 	}
+// // int main(int argc, char *argv[])
+// // {
+// // 	// printf("%d\n",check_is_number("78"));
+// // 	int i = 0;
+
+// //     int *arr = malloc((argc - 1) * sizeof(int));
+// // 	if (arr == NULL)
+// // 		exit(1);
+// // 	while (i < argc - 1)
+// // 	{
+// // 		// printf("%s\n",argv[i + 1]);
+// // 		if (!check_is_number(argv[i + 1]))
+// // 		{
+// // 			free(arr);
+// // 			exit(0);
+// // 		}
+// // 		arr[i] = atoi(argv[i + 1]);
+// // 		i++;
+// // 	}
+// // 	// printf("\n\n%d\n", arr[0]);
+// // 	printf("start printing the arr\n\n");
+// // 	print_arr(arr, i);
+// // 	return 0;
+// // }
+
+// //size
+// //capacity
+// //elements
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// typedef struct{
+// 	int *elements;
+// 	int capacity;
+// 	int size;
+// } Stack;
+
+// void print_elements(Stack *stack)
+// {
+// 	if (!stack) return ;
+// 	printf("hello\n");
+// }
+
+// int main(int argc, char const *argv[])
+// {
+// 	Stack *fir = malloc(sizeof(Stack));
+// 	fir->elements = malloc(sizeof(int) * 5);
+// 	fir->capacity = 5;
+// 	fir->size = 0;
+// 	printf("%d\n", fir->capacity);
+// 	print_elements(fir);
+// 	return 0;
+// }
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct s_stack
+{
+	int capacity;
+	int *stack;
+	int size;
+}				t_stack;
+
+void exit_error(char *err)
+{
+	
+}
+
+int check_argv(char **av)
+{
+	int i = 1;
+	int j = 0;
+	while (av[i])
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			if (!(av[i][j] >= '0' && av[i][j] <= '9'))
+				return 0;
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+	
+int check_dup(int *arr, int count)
+{
+	int i = 0;
+	int j = 0;
+	int temp;
+	while (i < count)
+	{
+		j = 0;
+		temp = arr[i];
+		while (j + 1 < count)
+		{
+			if (temp == arr[j + i+ 1])
+				return 0;
+			j++;
+		}
+		
+		i++;
+	}
+	return 1;
+}
+
+t_stack *fill_a(int ac, char **argv)
+{
+	int i = ac - 1;
+	// argv++;
+	if (!check_argv(argv)) return NULL;
+	t_stack *stack = malloc(sizeof(t_stack));
+	if (!stack)
+		return NULL;
+	stack->capacity = ac - 1;
+	stack->stack = malloc(sizeof(int) * ac - 1);
+	if (stack->stack == NULL)
+		return NULL;
+	int j = 0;
+	while (j < i)
+	{
+		stack->stack[j] = atoi(argv[j + 1]);
+		j++;
+	} 
+	// if(!check_dup(stack->stack, ac - 1)) return NULL;
+	if (check_dup(stack->stack,ac -1) == 1)
+	{
+		printf("no dup\n");
+	}else {
+		printf("there is dup\n");
+
+	}
+	return stack;
+}
+int main(int argc, char *argv[])
+{
+	// argv++;
+	// printf("%d\n", check_argv(argv));
+	// exit(1)
+;	t_stack *stack_a;
+	if (argc < 2)
+		return (-1);
+	stack_a = fill_a(argc, argv);
+	if(stack_a == NULL)
+		return 1;
+	printf("%d\n", stack_a->stack[0]);
+	return 0;
+}
