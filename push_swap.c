2dbfbb4687730d728bc5dc2680127e40b5b76ae0
Oussama Khourssa <okhourss@c3r9p9.1337.ma>
2025-01-25 19:34:20 +0100

    parsing moslty done

--- push_swap.c
+++ push_swap.c
@@ -5,17 +5,16 @@
 
 typedef struct s_stack
 {
-    int capacity;
     int *stack;
     int size;
     int top;
 } t_stack;
 
 // Function prototypes
-void pa(t_stack *stack_a, t_stack *stack_b, int print);
-void sa(t_stack *stack, int print);
-void sb(t_stack *stack, int print);
-void ss(t_stack *a, t_stack *b, int print);
+void pa(t_stack *stack_a, t_stack *stack_b);
+void sa(t_stack *stack);
+void sb(t_stack *stack);
+void ss(t_stack *a, t_stack *b);
 void exit_error(char *err);
 int *bubble_sort(int *arr, int count);
 int check_argv(char **av);
@@ -24,29 +23,36 @@ t_stack *fill_a(int ac, char **argv);
 void print_stack(t_stack *stack, char *name);
 
 // Push from stack B to stack A
-void pa(t_stack *stack_a, t_stack *stack_b, int print)
+void pa(t_stack *stack_a, t_stack *stack_b)
 {
     if (!stack_a || !stack_b)
         return;
     if (stack_b->size == 0)
         return;
-
-    // Push the top element of stack B to stack A
     stack_a->top--;
     stack_a->stack[stack_a->top] = stack_b->stack[stack_b->top];
     stack_a->size++;
 
-    // Pop the top element from stack B
     stack_b->top++;
     stack_b->size--;
 
-    // Print "pa" if required
-    if (print)
-        write(1, "pa\n", 3);
 }
 
+void pb(t_stack *stack_a, t_stack *stack_b)
+{
+    if (!stack_a || !stack_b)
+        return;
+    if (stack_a->size == 0)
+        return;
+    stack_b->top--;
+    stack_b->stack[stack_b->top] = stack_a->stack[stack_a->top];
+    stack_b->size++;
+
+    stack_a->top++;
+    stack_a->size--;
+}
 // Swap the first two elements of stack A
-void sa(t_stack *stack, int print)
+void sa(t_stack *stack)
 {
     if (!stack || stack->size < 2)
         return;
@@ -55,13 +61,10 @@ void sa(t_stack *stack, int print)
     stack->stack[stack->top] = stack->stack[stack->top + 1];
     stack->stack[stack->top + 1] = tmp;
 
-    // Print "sa" if required
-    if (print)
-        write(1, "sa\n", 3);
 }
 
 // Swap the first two elements of stack B
-void sb(t_stack *stack, int print)
+void sb(t_stack *stack)
 {
     if (!stack || stack->size < 2)
         return;
@@ -70,23 +73,16 @@ void sb(t_stack *stack, int print)
     stack->stack[stack->top] = stack->stack[stack->top + 1];
     stack->stack[stack->top + 1] = tmp;
 
-    // Print "sb" if required
-    if (print)
-        write(1, "sb\n", 3);
 }
 
 // Swap the first two elements of both stacks
-void ss(t_stack *a, t_stack *b, int print)
+void ss(t_stack *a, t_stack *b)
 {
     if (!a || !b)
         return;
 
-    sa(a, 0);
-    sb(b, 0);
-
-    // Print "ss" if required
-    if (print)
-        write(1, "ss\n", 3);
+    sa(a);
+    sb(b);
 }
 
 // Print an error message and exit
@@ -119,6 +115,48 @@ int *bubble_sort(int *arr, int count)
         i++;
     }
     return arr;
+}
+
+void ra(t_stack *stack_a)
+{
+    int hold;
+    int i = 0;
+    
+    hold = stack_a->stack[stack_a->size - 1];
+
+    while (i < stack_a->size - 1)
+    {
+        stack_a->stack[stack_a->size - i - 1] = stack_a->stack[stack_a->size - i - 2];
+        i++;
+    }
+    stack_a->stack[0] = hold;
+}
+
+int	ft_atoi(const char *nptr)
+{
+	size_t		i;
+	int			sign;
+	long			res;
+
+	i = 0;
+	res = 0;
+	sign = 1;
+	while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
+		i++;
+	if (nptr[i] == '+' || nptr[i] == '-')
+	{
+		if (nptr[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		res = res * 10 + nptr[i] - '0';
+        if ((res * sign) > INT_MAX || (res * sign) < INT_MIN)
+            exit_error("Error\n");
+		i++;
+	}
+	return (sign * res);
 }
 
 // Check if the arguments are valid numbers
@@ -173,7 +211,6 @@ t_stack *fill_a(int ac, char **argv)
     if (!stack)
         return NULL;
 
-    stack->capacity = ac - 1;
     stack->stack = malloc(sizeof(int) * (ac - 1));
     if (!stack->stack)
     {
@@ -183,9 +220,8 @@ t_stack *fill_a(int ac, char **argv)
 
     stack->top = 0;
     stack->size = ac - 1;
-
     for (int i = 0; i < ac - 1; i++)
-        stack->stack[i] = atoi(argv[i + 1]);
+        stack->stack[i] = ft_atoi(argv[i + 1]);
 
     if (!check_dup(stack->stack, ac - 1))
         exit_error("Error\n");
@@ -193,81 +229,45 @@ t_stack *fill_a(int ac, char **argv)
     return stack;
 }
 
+t_stack *initialize_b(t_stack *stack_a)
+{
+    if (!stack_a)
+        return NULL;
+    t_stack *stack_b = malloc(sizeof(t_stack));
+    if (!stack_b)
+    exit_error("Error\n");
+    stack_b->stack = malloc(sizeof(int) * stack_a->size);
+    if (!stack_b->stack)
+    {
+        free(stack_b);
+        exit_error("Error\n");
+    }
+    stack_b->size = 0;
+    stack_b->top = stack_a->size;
+    return stack_b;
+}
+
 // Print the elements of a stack
 void print_stack(t_stack *stack, char *name)
 {
-	printf("\nthis is the top -> %d#\n",stack->top);
     printf("%s: ", name);
     for (int i = stack->top; i < stack->size; i++)
         printf("%d ", stack->stack[i]);
     printf("\n");
 }
+
+
+
 int main(int argc, char *argv[])
 {
-	t_stack *stack_a;
-	t_stack *stack_b;
+    t_stack *stack_a;
+    t_stack *stack_b;
 
-	if (argc < 2)
-		return (-1);
-	stack_a = fill_a(argc, argv);
-	char *ag[] = {"a.out","5", "6", "7", "8", "9", NULL};
-	stack_b = fill_a(5, ag);
-	if(stack_a == NULL || stack_b == NULL)
-		return 1;
-	// stack_b->top = 0;
-	// stack_b->size = 5;
-	// stack_a->top = 0;
-	// stack_a->size = argc - 1;
-	// printf("Before sa: ");
-    // for (int i = stack_a->top; i < stack_a->size; i++)
-    //     printf("%d ", stack_a->stack[i]);
-    // printf("\n");
-	// 		printf("Before sb: ");
-    // for (int i = stack_b->top; i < stack_b->size; i++)
-    //     printf("%d ", stack_b->stack[i]);
-    // printf("\n");
-	// ss(stack_a, stack_b);
-	// // sa(stack_a);
-	// printf("after sa: ");
-    // for (int i = stack_a->top; i < stack_a->size; i++)
-    //     printf("%d ", stack_a->stack[i]);
-    // printf("\n");
-	// // ===-----------------------========
-	// // sb(stack_b);
-	// printf("after sb: ");
-    // for (int i = stack_b->top; i < stack_b->size; i++)
-    //     printf("%d ", stack_b->stack[i]);
-    // printf("\n");
-	// --------------------------------------------------
-	////////// ? test pa;
-	printf("stack b top element -> %d\n", stack_b->stack[stack_b->top]);
-	// printf("stack a before pa: ");
-	// for (int i = stack_a->top; i < stack_a->size; i++)
-    //     printf("%d ", stack_a->stack[i]);
-    // printf("\n");
-	print_stack(stack_a, "stack a before pa");
-	
-	pa(stack_a, stack_b, 0);
-	print_stack(stack_a, "stack a after  pa");
-	printf("size = %d\n", stack_a->size);
-	return 0;
+    if (argc < 2)
+        return -1;
+    stack_a = fill_a(argc, argv);
+    stack_b = initialize_b(stack_a);
+    print_stack(stack_a, "he:  -> ");
+    ra(stack_a);
+    print_stack(stack_a, "he:  -> ");
 }
-
-// -------------------- testing -----------------------
-
-
-
-// int main(int argc, char const *argv[])
-// {
-// 	int arr[] = {1, 3 ,5 ,2 ,9, 6,4};
-// 	int *res = bable_sort(arr,7);
-// 	for (size_t i = 0; i < 7; i++)
-// 	{
-// 		printf("%d",res[i]);
-// 		if (i != 6)
-// 		{
-// 			printf(", ");
-// 		}
-// 	}
-// 	return 0;
-// }
